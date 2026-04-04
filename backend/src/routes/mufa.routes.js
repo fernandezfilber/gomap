e5d9ced434090752f69f673b9f7afe576ae88903
Filber Fernandez <fernadezfilber2@gmail.com>
2026-04-04 08:07:53 -0500

    gol

--- backend/src/routes/mufa.routes.js
+++ backend/src/routes/mufa.routes.js
@@ -1,21 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const mufaController = require('../controllers/mufa.controller');
-// Quitamos el import de cajaController si ya movimos la lógica a mufa.controller
-const { verifyToken, isAdmin } = require('../Middleware/auth.middleware');
 
-// 1. Obtener todas
+// Sin middlewares por ahora para probar
 router.get('/', mufaController.getMufas);
-
-// 2. Obtener UNA (Elimina el "||", debe ser directo)
 router.get('/:id', mufaController.getMufaById); 
-
-// 3. Obtener ocupados (Usa el del mufaController que ya incluimos)
 router.get('/hilos-ocupados/:mufaId', mufaController.getHilosOcupados);
-
-// 4. Crear, Actualizar y Eliminar
 router.post('/', mufaController.crearMufa);
-router.put('/:id', mufaController.actualizarMufa);
+router.put('/:id', mufaController.actualizarMufa); // <--- LÍNEA 18
 router.delete('/:id', mufaController.eliminarMufa);
 
 module.exports = router;
