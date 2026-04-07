b830204c6b9b8926d8937b37912c86778893453b
Filber Fernandez <fernadezfilber2@gmail.com>
2026-04-06 23:49:35 -0500

    ultima de la noche

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,3 @@
-// src/db.js
 const { PrismaClient } = require('@prisma/client');
-
-const prisma = global.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
-
+const prisma = new PrismaClient();
 module.exports = prisma;
