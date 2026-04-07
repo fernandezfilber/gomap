be8bad69f68343d404b6e5ec44984ba4f67777f5
Filber Fernandez <fernadezfilber2@gmail.com>
2026-04-06 23:22:02 -0500

    actualizacion de prisma 7

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,13 +1,8 @@
 // src/db.js
 const { PrismaClient } = require('./generated/client');
 
-const prisma = global.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+// No pasamos parámetros, Prisma buscará DATABASE_URL en el entorno automáticamente
+const prisma = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
