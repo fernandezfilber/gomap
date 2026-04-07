caab3e93cdf8674a71987fd505c4b1bf4f81fda9
Filber Fernandez <fernadezfilber2@gmail.com>
2026-04-07 00:01:30 -0500

    est bien

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,20 +1,8 @@
+// src/db.js
 const { PrismaClient } = require('@prisma/client');
-const dotenv = require('dotenv');
-const path = require('path');
 
-// Forzamos la carga del .env (ruta absoluta hacia la raíz de nodejs)
-dotenv.config({ path: path.join(__dirname, '../.env') });
-
-const dbUrl = process.env.DATABASE_URL;
-
-const prisma = global.prisma || new PrismaClient({
-  datasources: {
-    db: {
-      url: dbUrl,
-    },
-  },
+const prisma = new PrismaClient({
+ log: ['error', 'warn'],
 });
-
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 
 module.exports = prisma;
