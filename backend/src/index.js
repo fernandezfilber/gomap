de6c159cf7bc6344dc43251a2045a66d92c42dd4
Filber Fernandez <fernadezfilber2@gmail.com>
2026-04-06 23:15:22 -0500

    actualizacion de prisma 7

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const { PrismaClient } = require("@prisma/client");
+const prisma = require('./db');
 
 // 1. Cargar variables de entorno
 dotenv.config();
